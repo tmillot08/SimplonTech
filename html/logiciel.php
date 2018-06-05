eb555d10830b196e8870215fda5e6d558b1b24d7
Inseri <39262186+Inseri@users.noreply.github.com>
2018-06-05 15:33:04 +0200

    Add files via upload

--- html/logiciel.php
+++ html/logiciel.php
@@ -3,10 +3,10 @@
 <head>
 <meta charset:"UTF-8">
 	<title>LOGICIELS</title>
-	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/header.css">
-	<link rel="stylesheet" href="css/logiciel.css">
+	<link rel="stylesheet" href="../css/reset.css">
+	<link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/header.css">
+	<link rel="stylesheet" href="../css/logiciel.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 </head>
 	<header>
