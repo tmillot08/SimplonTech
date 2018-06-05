b009ca82af3ce745372e3f8840dd5b7d63fa4811
Nexh1 <kevinponsart89@gmail.com>
2018-06-05 14:00:18 +0200

    yop

---
+++ kevin/index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- SCRIPTS JQUERY ET BOOTSTRAP -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <title>Titre</title>
+  </head>
+  <body>
+    <header>
+      <?php include('../html/header.html') ?>
+    </header>
+    <main class="main_produits">
+    <div class="container-fluid">
+
+      <h1>Nos produits</h1>
+
+      <h2>PC</h2>
+
+      <div class="row">
+        <div class="col-3">
+          <div class="card text-center">
+            <a href="https://google.com">
+            <div class="card-header p-5">Nom produit</div>
+            <div class="image_card1 card_image"></div>
+            <div class="card-body text-left p-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+              <div class="card-footer pt-4 pb-4">19€99</div>
+              </a>
+            </div>
+          </div>
+
+
+          <div class="col-3">
+            <div class="card text-center">
+              <a href="https://google.com">
+              <div class="card-header p-5">Nom produit</div>
+              <div  class="image_card2 card_image"></div>
+              <div class="card-body text-left p-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </div>
+                <div class="card-footer pt-4 pb-4">19€99</div>
+                </a>
+              </div>
+            </div>
+
+            <div class="col-3">
+              <div class="card text-center">
+                <a href="https://google.com">
+                <div class="card-header p-5">Nom produit</div>
+                <div class="image_card3 card_image"></div>
+                <div class="card-body text-left p-4">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </div>
+                  <div class="card-footer pt-4 pb-4">19€99</div>
+                  </a>
+                </div>
+              </div>
+
+              <div class="col-3">
+                <div class="card text-center">
+                  <a href="https://google.com">
+                  <div class="card-header p-5">Nom produit</div>
+                  <div class="image_card4" class="card_image"></div>
+                  <div class="card-body text-left p-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </div>
+                    <div class="card-footer pt-4 pb-4">19€99</div>
+                    </a>
+                  </div>
+                </div>
+
+      </div>
+
+      <h2>Catégorie 2</h2>
+
+      <div class="row">
+        <div class="col-3">
+          <div class="card text-center">
+            <a href="https://google.com">
+            <div class="card-header p-5">Nom produit</div>
+            <div class="image_card5" class="card_image"></div>
+            <div class="card-body text-left p-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+              <div class="card-footer pt-4 pb-4">19€99</div>
+              </a>
+            </div>
+          </div>
+
+
+          <div class="col-3">
+            <div class="card text-center">
+              <a href="https://google.com">
+              <div class="card-header p-5">Nom produit</div>
+              <div class="image_card6" class="card_image"></div>
+              <div class="card-body text-left p-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </div>
+                <div class="card-footer pt-4 pb-4">19€99</div>
+                </a>
+              </div>
+            </div>
+
+            <div class="col-3">
+              <div class="card text-center">
+                <a href="https://google.com">
+                <div class="card-header p-5">Nom produit</div>
+                <div class="image_card7" class="card_image"></div>
+                <div class="card-body text-left p-4">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </div>
+                  <div class="card-footer pt-4 pb-4">19€99</div>
+                  </a>
+                </div>
+              </div>
+
+              <div class="col-3">
+                <div class="card text-center">
+                  <a href="https://google.com">
+                  <div class="card-header p-5">Nom produit</div>
+                  <div class="image_card8" class="card_image"></div>
+                  <div class="card-body text-left p-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </div>
+                    <div class="card-footer pt-4 pb-4">19€99</div>
+                    </a>
+                  </div>
+                </div>
+
+      </div>
+
+      <h2>Catégorie 3</h2>
+
+      <div class="row">
+        <div class="col-3">
+          <div class="card text-center">
+            <a href="https://google.com">
+            <div class="card-header p-5">Nom produit</div>
+            <div class="image_card9" class="card_image"></div>
+            <div class="card-body text-left p-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+              <div class="card-footer pt-4 pb-4">19€99</div>
+              </a>
+            </div>
+          </div>
+
+
+          <div class="col-3">
+            <div class="card text-center">
+              <a href="https://google.com">
+              <div class="card-header p-5">Nom produit</div>
+              <div class="image_card10" class="card_image"></div>
+              <div class="card-body text-left p-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </div>
+                <div class="card-footer pt-4 pb-4">19€99</div>
+                </a>
+              </div>
+            </div>
+
+            <div class="col-3">
+              <div class="card text-center">
+                <a href="https://google.com">
+                <div class="card-header p-5">Nom produit</div>
+                <div class="image_card11" class="card_image"></div>
+                <div class="card-body text-left p-4">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </div>
+                  <div class="card-footer pt-4 pb-4">19€99</div>
+                  </a>
+                </div>
+              </div>
+
+              <div class="col-3">
+                <div class="card text-center">
+                  <a href="https://google.com">
+                  <div class="card-header p-5">Nom produit</div>
+                  <div class="image_card12" class="card_image"></div>
+                  <div class="card-body text-left p-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </div>
+                    <div class="card-footer pt-4 pb-4">19€99</div>
+                    </a>
+                  </div>
+                </div>
+
+      </div>
+    </div>
+    </main>
+
+    <footer>
+      <?php include("../html/footer.html") ?>
+    </footer>
+
+  </body>
+</html>
+
+<!-- NOS PRODUITS -->
