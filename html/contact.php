678b13c9b662ba36a6290286a656c6d6b8f584c0
Inseri <39262186+Inseri@users.noreply.github.com>
2018-06-06 10:12:07 +0200

    Add files via upload

--- html/contact.php
+++ html/contact.php
@@ -46,8 +46,8 @@
         <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1822.2214125532798!2d4.721833026858348!3d49.76706443812701!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f972394b33a06f%3A0xabefa42e8416115d!2sChambre+De+Commerce+Et+D&#39;Industrie+Des+Ardennes!5e0!3m2!1sfr!2sfr!4v1528123329443"
             width="50%" height="70%" frameborder="0" style="border:0"></iframe>
         <div class="conbox">
-            <p class="pcon"><span class="bold">Adresse :</span> 18 A Avenue Georges Corneau, 08000 Charleville-Mézières</p>
-            <p class="pcon"><span class="bold">Horaires d'ouvertures :</span>
+            <p class="pcon"><i class="fas fa-address-card ico"></i>  <span class="bold">Adresse :</span> 18 A Avenue Georges Corneau, 08000 Charleville-Mézières</p>
+            <p class="pcon"><i class="fas fa-clock ico"></i>  <span class="bold">Horaires d'ouvertures :</span>
                 <?php
 		 					if (date('w') == 0 || date('w') == 6){
 		 						echo ('<span class="fer"> Le magasin est fermé</span>');}
@@ -57,9 +57,9 @@
 								echo ('<span class="ouv"> Le magasin est ouvert.</span>');
 							}
 						?>
-                <br> Lundi : 9h00-18h00<br> Mardi : 9h00-18h00<br> Mercredi : 9h00-18h00<br> Jeudi : 9h00-18h00<br> Vendredi : 9h00-18h00<br></p>
-            <p class="pcon"><span class="bold">Téléphone :</span> 03-01-02-04-05</p>
-            <p class="pcon"><span class="bold">Mail :</span> test.test@test.fr</p>
+                <br><br> Lundi : 9h00-18h00<br> Mardi : 9h00-18h00<br> Mercredi : 9h00-18h00<br> Jeudi : 9h00-18h00<br> Vendredi : 9h00-18h00<br></p>
+            <p class="pcon"><i class="fas fa-phone ico"></i>  <span class="bold">Téléphone :</span> 03-01-02-04-05</p>
+            <p class="pcon"><i class="fas fa-envelope-open ico"></i>  <span class="bold">Mail :</span> test.test@test.fr</p>
         </div>
     </div>
     </mains>
