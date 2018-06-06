fde28d2b8d6e2a89241d464fcd4db41ae190fd5f
tmillot08 <thomas.millot08@gmail.com>
2018-06-06 16:37:03 +0200

    dhee

--- html/accueil.php
+++ html/accueil.php
@@ -3,12 +3,10 @@
   <head>
     <meta charset="utf-8">
     <title>Accueil</title>
-    <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/accueil.css">
-    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/accueil.css">
   </head>
   <body>
     <header>
