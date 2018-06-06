e65dc5b176e5a316900cdcd457b1cae8bfca41aa
Nexh1 <kevinponsart89@gmail.com>
2018-06-06 12:17:10 +0200

    POULE

--- html/presentation.php
+++ html/presentation.php
@@ -2,8 +2,10 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/presentation.css">
+    <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/presentation.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <title>Presentation</title>
   </head>
   <body>
