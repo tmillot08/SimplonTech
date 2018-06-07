d4c5c06fd6ec1cbebabde87c9977374827828e42
Inseri <39262186+Inseri@users.noreply.github.com>
2018-06-07 17:19:49 +0200

    Add files via upload

---
+++ logiciel.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset:"UTF-8">
+	<title>LOGICIELS</title>
+	<link rel="stylesheet" href="../css/reset.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/logiciel.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+</head>
+	<header>
+	<?php include 'header.html'?>
+</header>
+<main>
+            <div class="logiCard">
+                <div class="logiInfo">
+                    <img class="pict" src="../img/win10h.jpg">
+                    <p class="txt">Windows 10 familial 32 bits/64bits est compatible avec la majorité de vos logiciels et vos périphériques. Découvrez Cortana, l’assistante numérique personnelle Microsoft, profitez de navigateur Microsoft Edge et Jouez aux jeux Xbox One sur les appareils Windows 10.</p>
+                    <p class="btn">Acheter<br><br><i class="fas fa-cart-arrow-down icoach"></i><br><br>>120.00€<</p>
+                    <p class="btn">Plus d'Info<br><br><i class="fas fa-file-alt icoinf"></i></p>
+                </div>
+            </div>
+            <div class="logiCard">
+                <div class="logiInfo">
+                    <img class="pict" src="../img/win10p.jpg">
+                    <p class="txt">Windows 10 professionnel 32 bits/64bits est compatible avec la majorité de vos logiciels et vos périphériques. Découvrez Cortana, l’assistante numérique personnelle Microsoft, profitez de navigateur Microsoft Edge et profitez des dernières fonctionnalités de sécurité pour votre entreprise.</p>
+                    <p class="btn">Acheter<br><br><i class="fas fa-cart-arrow-down icoach"></i><br><br>>220.00€<</p>
+                    <p class="btn">Plus d'Info<br><br><i class="fas fa-file-alt icoinf"></i></p>
+                </div>
+            </div>
+            <div class="logiCard">
+                <div class="logiInfo">
+                    <img class="pict" src="../img/office2016.jpg">
+                    <p class="txt">Osez la différence avec Office 2016 et découvrez les nouvelles fonctionnalités ! Collaboration intensive avec votre équipe. Accédez à vos documents partout sur tous vos appareils.</p>
+                    <p class="btn">Acheter<br><br><i class="fas fa-cart-arrow-down icoach"></i><br><br>>150.00€<</p>
+                    <p class="btn">Plus d'Info<br><br><i class="fas fa-file-alt icoinf"></i></p>
+                </div>
+            </div>
+            <div class="logiCard">
+                <div class="logiInfo">
+                    <img class="pict" src="../img/photoshop.png">
+                    <p class="txt">Adobe Photoshop a su rester, au gré des version, la référence dans le domaine de la retouche et de la création numérique. Un outil professionnel surpuissant.</p>
+                    <p class="btn">Acheter<br><br><i class="fas fa-cart-arrow-down icoach"></i><br><br>>350.00€<</p>
+                    <p class="btn">Plus d'Info<br><br><i class="fas fa-file-alt icoinf"></i></p>
+                </div>
+            </div>
+            <div class="logiCard">
+                <div class="logiInfo">
+                    <img class="pict" src="../img/nod32.jpg">
+                    <p class="txt">Profitez d'une machine sûre grâce au meilleur anti-virus du marché, le tout à un prix très raisonnable. Ce pack comprend 3 licences afin de protèger 3 postes !</p>
+                    <p class="btn">Acheter<br><br><i class="fas fa-cart-arrow-down icoach"></i><br><br>>14.00€<</p>
+                    <p class="btn">Plus d'Info<br><br><i class="fas fa-file-alt icoinf"></i></p>
+                </div>
+            </div>
+            <div class="logiCard">
+                <div class="logiInfo">
+                    <img class="pict" src="../img/7zip.png">
+                    <p class="txt">7-Zip permet de compresser vos dossiers et vos fichiers afin qu'ils utilisent moins d'espace sur votre disque dur. Le programme est capable de créer des archives aux formats 7z, TAR, Gzip, Bzip2 et ZIP. Il peut ouvrir de nombreux formats tels que 7z, ZIP, CAB, RAR, ARJ, Gzip, Bzip2, TAR, Cpio, RPM et DEB.</p>
+                    <a href="../dl/7z1805-x64.exe"><p class="btn">GRATUIT<br><br><i class="fas fa-cart-arrow-down icoach"></i><br><br>>DOWNLOAD<</p></a>
+                    <p class="btn">Plus d'Info<br><br><i class="fas fa-file-alt icoinf"></i></p>
+                </div>
+            </div>        
+						</main>
+			<footer>
+					<?php include 'footer.html'?>
+					
+            </footer>
+            <script type="text/javascript" src="../js/selecteur.js"></script>
+	</body>
+</html> 
