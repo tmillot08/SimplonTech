3c2fd69d51e1dcc356da8d6f1e031270fdba6ba6
Inseri <39262186+Inseri@users.noreply.github.com>
2018-06-08 11:38:56 +0200

    Update 7zip.php

--- html/7zip.php
+++ html/7zip.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset:"UTF-8">
-	<title>LOGICIELS</title>
+	<title>7ZIP</title>
 	<link rel="stylesheet" href="../css/reset.css">
 	<link rel="stylesheet" href="../css/style.css">
 	<link rel="stylesheet" href="../css/7zip.css">
