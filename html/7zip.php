5b89cbae2e80594fb2d54c39dffb768699898132
Inseri <39262186+Inseri@users.noreply.github.com>
2018-06-08 11:33:49 +0200

    Add files via upload

---
+++ html/7zip.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset:"UTF-8">
+	<title>LOGICIELS</title>
+	<link rel="stylesheet" href="../css/reset.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/7zip.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+</head>
+	<header>
+	<?php include 'header.html'?>
+</header>
+<main>
+            <div class="logiCard">
+                <div class="logiInfo">
+                    <img class="pict" src="../img/7ziphead.jpg">
+                    <a class="btn btna" href="../dl/7z1805-x64.exe"><p>GRATUIT<br><br><i class="fas fa-cart-arrow-down icoach"></i><br><br>>DOWNLOAD<</p></a>
+                    <a class="brn btna" id="bar" href="../html/logiciel.php"><p class="btn">Retour Liste<br><br><i class="fas fa-arrow-alt-circle-left icoinf"></i></p><a>
+                </div>
+                <div class="framework">
+                    <div class ="onglet">
+                        <h2>PRESENTATION</h2>
+                    </div>
+
+                    <div>
+                        <p class="txt2">7-Zip est un logiciel de compression de données et d’archivage de fichiers fonctionnant sous Windows développé par le Russe Igor Pavlov.<br> Il peut également être utilisé avec d’autres systèmes d’exploitation (GNU/Linux…) grâce, par exemple, au projet Wine ou au portage de sa version console sous GNU/Linux, nommé p7zip.<br>
+                        C’est un logiciel libre distribué sous licence LGPL, le code LZMA est dans le domaine public, le code AES est sous licence BSD et le code unRAR est sous licence mixte.<br>Vous pouvez utiliser 7-Zip sur n'importe quel ordinateur, y compris dans les sociétés commerciales.<br> Il n'est pas nécessaire de s'enregistrer ou payer pour utiliser 7-Zip.</p>
+                    </div>
+
+                    <div class ="onglet">
+                     <h2>FONCTIONNALITES</h2>
+                 </div>
+
+                   <div>                  
+                        <p class="txt3">Taux de compression très élevé du nouveau format 7z avec compression LZMA et LZMA2.<br>
+                         Formats supportés:<br>
+                        Compression et décompression: 7z, XZ, BZIP2, GZIP, TAR, ZIP et WIM<br>
+                         Uniquement décompression: AR, ARJ, CAB, CHM, CPIO, CramFS, DMG, EXT, FAT, GPT, HFS, IHEX, ISO, LZH, LZMA, MBR, MSI, NSIS, NTFS, QCOW2, RAR, RPM, SquashFS, UDF, UEFI, VDI, VHD, VMDK, WIM, XAR et Z.<br>
+                        Pour les formats ZIP et GZIP, 7-Zip fourni un taux de compression qui est 2-10 % meilleur que le taux de PKZip et WinZip<br>
+                        Chiffrement fort AES-256 pour les formats 7z et ZIP<br>
+                        Capacité d'auto extraction avec le format 7z<br>
+                        Intégration au système Windows<br>
+                        Gestionnaire de fichier puissant<br>
+                        Version en ligne de commande puissante<br>
+                        Plugin pour le gestionnaire FAR<br>
+                        Traduit en 87 langues<br>
+                        7-Zip fonctionne sous Windows 10 / 8 / 7 / Vista / XP / 2012 / 2008 / 2003 / 2000 / NT. Il existe une version en ligne de commande pour Linux/Unix.
+                        </p>
+                    </div>
+
+                    <div class ="onglet">
+                        <h2>UN TAUX DE COMPRESSION INEGALE !!!</h2>
+                    </div>
+
+                    <div>
+                        <p class="txt2">
+                        Nous avons comparé 7-Zip avec quelques-uns des meilleurs logiciels d'archivage du marché.<br>
+                        Jeux de fichiers: Mozilla Firefox 34.0.5 for Windows and Google Earth 6.2.2.6613 for Windows</p>
+                            <img class="pict2" src="../img/chart.png">
+                        <p class="txt2">Les résultats des taux de compression dépendent beaucoup des données utilisées pour les tests.<br>
+                        Normalement 7-Zip compresse 30-70% mieux en format 7z qu'en format zip.<br> Et 7-Zip compresse 2-10% mieux en format zip que les autres logiciels d'archivage compatibles avec le format zip.</p>
+                    </div>
+
+                    <div class ="onglet" id="rtn">
+                        <a id="bar" href="7zip.php#"><h2>RETOUR HAUT DE PAGE</h2></a>
+                    </div>
+                </div>
+            </div>       
+						</main>
+			<footer>
+					<?php include 'footer.html'?>
+					
+            </footer>
+            <script type="text/javascript" src="../js/selecteur.js"></script>
+	</body>
+</html> 
